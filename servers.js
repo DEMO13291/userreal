46c5a9ecfd8e5cd6fff223fc31cf03a9aa8394c2
DEMO13291 <wd40ishim@gmail.com>
2025-03-23 23:10:39 -0400

    Create servers.js

---
+++ servers.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const signupWebhook = 'https://discord.com/api/webhooks/1353564689170300928/9Lw4gYGvjxZ6jLWJWWjJ6Ehuez0hXwBIdAvZ0LKI2g5NDNo_dtwB6DCVWIams0JeqBWb';
+const banWebhook = 'https://discord.com/api/webhooks/1353564876051714251/_3ck-pBEYWeEUyk6yFqFaqvfJ3EYnYweZ7icCZm45zwuWQm8moqCw3AkJAO2gbhr8s_X';
+
+let users = [];
+
+if (fs.existsSync('./users.json')) {
+  users = JSON.parse(fs.readFileSync('./users.json'));
+}
+
+function saveUsers() {
+  fs.writeFileSync('./users.json', JSON.stringify(users, null, 2));
+}
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+  if (users.find(u => u.username === username)) {
+    return res.json({ success: false, message: 'Username already exists!' });
+  }
+
+  users.push({ username, password, banned: false });
+  saveUsers();
+
+  // Send signup info to Discord webhook
+  await axios.post(signupWebhook, {
+    content: `🔑 **New User Signed Up:**\n\`\`\`Username: ${username}\nPassword: ${password}\`\`\``,
+    components: [{
+      type: 1,
+      components: [{
+        type: 2,
+        style: 4, // Red button
+        label: 'BAN',
+        custom_id: `ban_${username}`
+      },{
+        type: 2,
+        style: 3, // Green button
+        label: 'UNBAN',
+        custom_id: `unban_${username}`
+      }]
+    }]
+  });
+
+  res.json({ success: true });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username === username && u.password === password);
+  
+  if (!user) return res.json({ success: false, message: 'Invalid username/password' });
+  if (user.banned) return res.json({ success: false, message: 'You have been banned.' });
+
+  res.json({ success: true, message: 'Successfully logged in!' });
+});
+
+// Simple endpoints for Discord bot (ban/unban, if used later)
+app.post('/ban', (req, res) => {
+  const { username } = req.body;
+  const user = users.find(u => u.username === username);
+  if (user) {
+    user.banned = true;
+    saveUsers();
+    axios.post(banWebhook, { content: `🚫 User **${username}** has been banned.` });
+    res.json({ success: true });
+  } else res.json({ success: false });
+});
+
+app.post('/unban', (req, res) => {
+  const { username } = req.body;
+  const user = users.find(u => u.username === username);
+  if (user) {
+    user.banned = false;
+    saveUsers();
+    axios.post(banWebhook, { content: `✅ User **${username}** has been unbanned.` });
+    res.json({ success: true });
+  } else res.json({ success: false });
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
